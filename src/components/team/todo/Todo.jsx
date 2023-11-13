ee7a46c61335776115c768c6332223ede30d7145
chysis <chysiss@naver.com>
2023-11-13 23:23:59 +0900

    ✨ Feat: todo 필드 구현

---
+++ src/components/team/todo/Todo.jsx
@@ -0,0 +1,42 @@
+import { s } from "./style.jsx";
+import TeamCommonAlert from "./../teamCommon/TeamCommonAlert";
+import TeamCommonSectionTitle from "../teamCommon/TeamCommonSectionTitle.jsx";
+import TodoEntry from "../../todoEntry/TodoEntry.jsx";
+
+function Todo({ today, unfinished }) {
+  const todayTasks = today.map(t => (
+    <TodoEntry key={t.id} category={t.category} title={t.title} col={t.color} />
+  ));
+
+  const unfinishedTasks = unfinished.map(u => (
+    <TodoEntry key={u.id} category={u.category} title={u.title} col={u.color} />
+  ));
+
+  const selectedDate = "선택한 날짜";
+  const unfinishedDescription = "아직 완료되지 않았어요";
+
+  return (
+    <s.TodoWrapper>
+      <TeamCommonSectionTitle title="Todo" />
+      <TeamCommonAlert />
+      <s.TodoButtonsWrapper></s.TodoButtonsWrapper>
+      <s.TodoContentsWrapper>
+        {/* 캘린더 */}
+        <s.TodoListsWrapper>
+          <s.Today>
+            <s.TodayDate>{selectedDate}</s.TodayDate>
+            {todayTasks}
+          </s.Today>
+          <s.UnfinishedTasks>
+            <s.UnfinishedDescription>
+              {unfinishedDescription}
+            </s.UnfinishedDescription>
+            {unfinishedTasks}
+          </s.UnfinishedTasks>
+        </s.TodoListsWrapper>
+      </s.TodoContentsWrapper>
+    </s.TodoWrapper>
+  );
+}
+
+export default Todo;
