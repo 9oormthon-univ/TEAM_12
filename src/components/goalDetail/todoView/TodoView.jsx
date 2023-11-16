7d3732b1315a12d37a6c5ffdcfd2926ccc181e0c
chysis <chysiss@naver.com>
2023-11-17 02:08:38 +0900

    🚧 feat: goal 사이드바 todoView 컴포넌트 생성, 구현 방법 수정 필요

---
+++ src/components/goalDetail/todoView/TodoView.jsx
@@ -0,0 +1,18 @@
+import TodoEntry from "../../todo/todoEntry/TodoEntry.jsx";
+import { s } from "./style.jsx";
+
+function TodoView({ todoList }) {
+  // const processData = todoList.map(todo => (
+  //   <TodoEntry
+  //     key={todo.todoId}
+  //     category={todo.goalContent}
+  //     title={todo.todoContent}
+  //     col={todo.todoManager}
+  //     //ischecked={todo.isComplete}
+  //   />
+  // ));
+
+  return <s.todoListWrapper>HELLO</s.todoListWrapper>;
+}
+
+export default TodoView;
---
+++ src/components/goalDetail/todoView/style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const s = {
+  todoListWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.625rem;
+    align-self: stretch;
+  `
+};
