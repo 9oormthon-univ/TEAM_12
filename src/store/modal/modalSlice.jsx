7c1372f29cfab122fa0d41dd7dd3fd2194a3f04c
yusuShin <tlsdbtn0507@naver.com>
2023-11-18 02:23:18 +0900

    :memo: [todo] todo제작을 위한 정보들 리덕스툴킷 썽크 활용
    리덕스에 정보를 담아 제작 모달을 활용하므로 투두 제작을 위한 정보를 썽크를 이용해 담음

--- src/store/modal/modalSlice.jsx
+++ src/store/modal/modalSlice.jsx
@@ -1,4 +1,5 @@
-import { createSlice , getDefaultMiddleware} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice , getDefaultMiddleware} from "@reduxjs/toolkit";
+import { API } from "../../api/axios";
 
 const initialState = {
     showModal: false,
@@ -6,6 +7,20 @@ const initialState = {
     title:'',
     addInfo:{}
 }
+
+export const todoGetMemList = createAsyncThunk(
+    'modalSlice/todoGetMemList',
+    async (projectId)=>{
+        return {res} = await API.get(`members/${projectId}`); 
+    }
+)
+
+export const todoGetGoalList = createAsyncThunk(
+    'modalSlice/todoGetGoalList',
+    async (projectId) => {
+        return {res} = await API.get(`projects/${projectId}/goals`);
+    }
+)
 
 const modalSlice = createSlice({
     name:'modal',
@@ -24,10 +39,25 @@ const modalSlice = createSlice({
             state.addInfo = {}
         }
     },
+    extraReducers:(builder)=>{
+        builder.addCase(todoGetMemList.fulfilled,(state,action)=>{
+            state.addInfo.managers = action.payload;
+        }),
+        builder.addCase(todoGetGoalList.fulfilled,(state,action)=>{
+            state.addInfo.goals = action.payload.map(e=>e.title);
+            state.addInfo.duration = [
+                action.payload.map(e=>e.startDate),
+                action.payload.map(e=>e.endDate)
+            ];
+        })
+    },
     middleware: getDefaultMiddleware({
         serializableCheck: false,
       }),
 })
+
+
+
 
 export const modalAction = modalSlice.actions
 
