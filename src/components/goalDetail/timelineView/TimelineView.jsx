a5af0d2904bced4d3f69a01714dbd07b04ce30ba
chysis <chysiss@naver.com>
2023-11-18 04:05:28 +0900

    🐛 fix: 값이 저장되기 전에 렌더링되는 현상 수정

--- src/components/goalDetail/timelineView/TimelineView.jsx
+++ src/components/goalDetail/timelineView/TimelineView.jsx
@@ -3,39 +3,45 @@ import { s } from "./style.jsx";
 function TimelineView({ timelineData, goalName }) {
   const ColorList = [null, "green", "blue"];
 
-  const processGoalComment = timelineData.goalCommentList.map(comment => (
-    <s.GoalComment $color={ColorList[comment.commentMemberId]}>
-      <s.GoalCommentDate>{comment.createdTime}</s.GoalCommentDate>
-      <s.GoalCommentContent>{comment.content}</s.GoalCommentContent>
-      <s.GoalCommentFeelingWrapper>"EMPTY"</s.GoalCommentFeelingWrapper>
-    </s.GoalComment>
-  ));
+  const processGoalComment =
+    timelineData.goalCommentList &&
+    timelineData.goalCommentList.map(comment => (
+      <s.GoalComment $color={ColorList[comment.commentMemberId]}>
+        <s.GoalCommentDate>{comment.createdTime}</s.GoalCommentDate>
+        <s.GoalCommentContent>{comment.content}</s.GoalCommentContent>
+        <s.GoalCommentFeelingWrapper>"EMPTY"</s.GoalCommentFeelingWrapper>
+      </s.GoalComment>
+    ));
 
-  const processTodo = timelineData.todoCommentList.map(todo => (
-    <s.TodoWrapper key={todo.todoId}>
-      <s.TodoBox $color={ColorList[todo.todoManagerMemberId]}>
-        <s.ChkBox>
-          <s.ChkBoxLabel $color={ColorList[todo.todoManagerMemberId]}>
-            ✔
-          </s.ChkBoxLabel>
-        </s.ChkBox>
-        <s.TodoBoxContentWrapper>
-          <s.TodoTitle>{goalName}</s.TodoTitle>
-          <s.TodoContent>{todo.todoContent}</s.TodoContent>
-        </s.TodoBoxContentWrapper>
-      </s.TodoBox>
-      <s.TodoCommentsWrapper>
-        {todo.commentList.map(comment => {
-          return (
-            <s.TodoComment>
-              <s.TodoCommentContent>{comment.content}</s.TodoCommentContent>
-              <s.TodoCommentFeelingWrapper>"EMPTY"</s.TodoCommentFeelingWrapper>
-            </s.TodoComment>
-          );
-        })}
-      </s.TodoCommentsWrapper>
-    </s.TodoWrapper>
-  ));
+  const processTodo =
+    timelineData.todoCommentList &&
+    timelineData.todoCommentList.map(todo => (
+      <s.TodoWrapper key={todo.todoId}>
+        <s.TodoBox $color={ColorList[todo.todoManagerMemberId]}>
+          <s.ChkBox>
+            <s.ChkBoxLabel $color={ColorList[todo.todoManagerMemberId]}>
+              ✔
+            </s.ChkBoxLabel>
+          </s.ChkBox>
+          <s.TodoBoxContentWrapper>
+            <s.TodoTitle>{goalName}</s.TodoTitle>
+            <s.TodoContent>{todo.todoContent}</s.TodoContent>
+          </s.TodoBoxContentWrapper>
+        </s.TodoBox>
+        <s.TodoCommentsWrapper>
+          {todo.commentList.map(comment => {
+            return (
+              <s.TodoComment>
+                <s.TodoCommentContent>{comment.content}</s.TodoCommentContent>
+                <s.TodoCommentFeelingWrapper>
+                  "EMPTY"
+                </s.TodoCommentFeelingWrapper>
+              </s.TodoComment>
+            );
+          })}
+        </s.TodoCommentsWrapper>
+      </s.TodoWrapper>
+    ));
 
   return (
     <s.TimelineWrapper>
