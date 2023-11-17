b207118732feb7e9e3be4d8374d30c85815d4d47
chysis <chysiss@naver.com>
2023-11-17 21:26:15 +0900

    🚧 goal 사이드바 timeline

--- src/components/goalDetail/timelineView/TimelineView.jsx
+++ src/components/goalDetail/timelineView/TimelineView.jsx
@@ -1,3 +1,56 @@
-function TimelineView() {}
+import { s } from "./style.jsx";
+
+function TimelineView({ timelineData, goalName }) {
+  // todo마다 isComplete 속성 필요
+
+  const ColorList = [null, "green", "blue"];
+
+  const processGoalComment = timelineData.goalCommentList.map(comment => (
+    <s.GoalComment $color={ColorList[comment.commentMemberId]}>
+      {console.log(comment)}
+      <s.GoalCommentDate>{comment.createdTime}</s.GoalCommentDate>
+      <s.GoalCommentContent>{comment.content}</s.GoalCommentContent>
+      <s.GoalCommentFeelingWrapper>"EMPTY"</s.GoalCommentFeelingWrapper>
+    </s.GoalComment>
+  ));
+
+  const processTodo = timelineData.todoCommentList.map(todo => (
+    <s.TodoWrapper key={todo.todoId}>
+      <s.TodoBox>
+        {/** 체크박스 */}
+        <s.TodoBoxContentWrapper>
+          <s.TodoTitle>{goalName}</s.TodoTitle>
+          <s.TodoContent>{todo.todoContent}</s.TodoContent>
+        </s.TodoBoxContentWrapper>
+      </s.TodoBox>
+      <s.TodoCommentsWrapper>
+        {todo.commentList.map(comment => {
+          return (
+            <s.TodoComment>
+              <s.TodoCommentContent>{comment.content}</s.TodoCommentContent>
+              <s.TodoCommentFeelingWrapper>"EMPTY"</s.TodoCommentFeelingWrapper>
+            </s.TodoComment>
+          );
+        })}
+      </s.TodoCommentsWrapper>
+    </s.TodoWrapper>
+  ));
+
+  return (
+    <s.TimelineWrapper>
+      <s.GoalCommentsWrapper>
+        {timelineData.goalCommentList.map(comment => (
+          <s.GoalComment $color={ColorList[comment.commentMemberId]}>
+            {console.log(comment)}
+            <s.GoalCommentDate>{comment.createdTime}</s.GoalCommentDate>
+            <s.GoalCommentContent>{comment.content}</s.GoalCommentContent>
+            <s.GoalCommentFeelingWrapper>"EMPTY"</s.GoalCommentFeelingWrapper>
+          </s.GoalComment>
+        ))}
+      </s.GoalCommentsWrapper>
+      {processTodo}
+    </s.TimelineWrapper>
+  );
+}
 
 export default TimelineView;
--- src/pages/goal/Goal.jsx
+++ src/pages/goal/Goal.jsx
@@ -13,7 +13,7 @@ function Goal() {
   const param = useParams();
   const DUMMY_TODOLIST = [
     {
-      goalId:1,
+      goalId: 1,
       goalContent: "FE 개발 1차 마감",
       todoId: 1,
       todoContent: "도메인 구매하기",
@@ -21,7 +21,7 @@ function Goal() {
       isComplete: false
     },
     {
-      goalId:2,
+      goalId: 2,
       goalContent: "FE 개발 1차 마감",
       todoId: 2,
       todoContent: "Progress 디자인 수정 반영",
@@ -29,7 +29,7 @@ function Goal() {
       isComplete: false
     },
     {
-      goalId:3,
+      goalId: 3,
       goalContent: "FE 개발 1차 마감",
       todoId: 3,
       todoContent: "TeamPage member 초대 오류 수정하기 2",
@@ -37,7 +37,7 @@ function Goal() {
       isComplete: true
     },
     {
-      goalId:4,
+      goalId: 4,
       goalContent: "FE 개발 1차 마감",
       todoId: 4,
       todoContent: "TeamPage member 초대 오류 수정하기",
@@ -46,7 +46,47 @@ function Goal() {
     }
   ];
 
+  const DUMMY_TIMELINE = {
+    goalCommentList: [
+      {
+        createdTime: "2023-11-15T21:25:52.560149",
+        content: "todo를 완성하는데 어려움이 있네요요",
+        commentMemberId: 1,
+        emojiCount: 2,
+        feelingCheckMemberId: [2, 1]
+      }
+    ],
+    todoCommentList: [
+      {
+        todoId: 3,
+        todoContent: "개발 시작11",
+        todoEndDate: "2023-11-15",
+        finishDate: "2023-11-17T00:34:46.842115",
+        todoManagerMemberId: 1,
+        commentList: [
+          {
+            createdTime: "2023-11-15T21:31:30.298951",
+            content: "todo를 완성하는데 어려움이 있네요요",
+            commentMemberId: 1,
+            emojiCount: 2,
+            feelingCheckMemberId: [2, 1]
+          }
+        ]
+      },
+      {
+        todoId: 4,
+        todoContent: "개발 시작22",
+        todoEndDate: "2023-11-30",
+        finishDate: "2023-11-17T00:34:55.886765",
+        todoManagerMemberId: 1,
+        commentList: []
+      }
+    ]
+  };
+
   const [activeTab, setActiveTab] = useState("Timeline");
+
+  const GoalTitle = DUMMY_TODOLIST[0].goalContent;
 
   const TabClickHandler = tab => {
     setActiveTab(tab);
@@ -79,7 +119,12 @@ function Goal() {
               <s.NavLine />
             </s.NavBarWrapper>
             <s.ViewWrapper>
-              {activeTab === "Timeline" && <TimelineView />}
+              {activeTab === "Timeline" && (
+                <TimelineView
+                  goalName={GoalTitle}
+                  timelineData={DUMMY_TIMELINE}
+                />
+              )}
               {activeTab === "Todo" && <TodoView todoList={DUMMY_TODOLIST} />}
             </s.ViewWrapper>
             <TeamCommonCommentPost />
