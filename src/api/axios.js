0fda8ba66b84ed7b9fb571e99dd58f6a8b2e7abc
yusuShin <tlsdbtn0507@naver.com>
2023-11-18 16:38:58 +0900

    :memo: [URL] url 전역변수 처리
    뭔가좀 있어보이는 효과를 줍니다

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const REACT_APP_GOORM_API = 'http://15.165.143.16:8080'
+
 export const API = axios.create({
-  baseURL: "http://15.165.143.16:8080",
+  // baseURL: import.meta.env.REACT_APP_GOORM_API,
+  baseURL: REACT_APP_GOORM_API,
   headers: {
     "Content-Type": "application/json"
   }
