3513158f545b7c9c5cae4c49bc75a68511b36fb1
chysis <chysiss@naver.com>
2023-11-18 23:08:58 +0900

    ✨ feat: goal 사이드바 디데이, 퍼센트 구현

--- src/components/goalDetail/top/Top.jsx
+++ src/components/goalDetail/top/Top.jsx
@@ -1,10 +1,40 @@
 import { s } from "./style.jsx";
 import TeamCommonCheckField from "./../../team/teamCommon/TeamCommonCheckField";
+import moment from "moment";
 
-function Top() {
+function Top({ data }) {
   const chkText = "Goal 달성에 동의하기";
-  const goalDday = "D-13";
-  const goalPercent = "100%";
+  let goalDday;
+  let goalPercent;
+
+  const goalDdayMaker = () => {
+    const today = moment();
+    const endDate = moment(data.data.endDate);
+    const answer = moment.duration(endDate.diff(today, "days"));
+    if (answer > 0) {
+      goalDday = `D-${answer}`;
+    } else if (answer === 0) {
+      goalDday = `D-day`;
+    } else if (answer < 0) {
+      goalDday = `D+${-answer}`;
+    }
+  };
+
+  const goalPercentMaker = () => {
+    let falseAmount = 0;
+    let trueAmount = 0;
+    data.data.todoList.map(todo => {
+      if (todo.isComplete === false) {
+        falseAmount++;
+      } else trueAmount++;
+    });
+    goalPercent = `${parseInt(
+      (trueAmount / (falseAmount + trueAmount)) * 100
+    )}%`;
+  };
+
+  goalDdayMaker();
+  goalPercentMaker();
 
   return (
     <s.Top>
